f412cc5db8a9e00a3152759f1f1dd086512ef5e5
Catherine Liu <catherine.liu@elastic.co>
2018-10-16 16:11:34 -0400

    Added more templates to test with

--- x-pack/plugins/canvas/public/components/workpad_templates/templates/index.js
+++ x-pack/plugins/canvas/public/components/workpad_templates/templates/index.js
@@ -7,7 +7,11 @@
 import { cloneDeep } from 'lodash';
 const canvasWeather = require('./canvas_weather.json');
 const conditionalAssets = require('./conditional_assets.json');
+const dublinDemo = require('./dublin_eah_workshop.json');
+const ecommerceRevenueTracking = require('./ecommerce_revenue_tracking.json');
 const elasticTemplate = require('./elastic_template.json');
+const flightsOverview = require('./flights_overview.json');
+const logsWebTraffic = require('./logs_web_traffic.json');
 const markdownStyles = require('./markdown_styles.json');
 const operationalMonitoring = require('./sample_data_operational_monitoring.json');
 const randomizedTour2018 = require('./randomized_tour_2018.json');
@@ -15,7 +19,11 @@ const randomizedTour2018 = require('./randomized_tour_2018.json');
 export const templates = [
   cloneDeep(canvasWeather),
   cloneDeep(conditionalAssets),
+  cloneDeep(dublinDemo),
+  cloneDeep(ecommerceRevenueTracking),
   cloneDeep(elasticTemplate),
+  cloneDeep(flightsOverview),
+  cloneDeep(logsWebTraffic),
   cloneDeep(markdownStyles),
   cloneDeep(operationalMonitoring),
   cloneDeep(randomizedTour2018),
