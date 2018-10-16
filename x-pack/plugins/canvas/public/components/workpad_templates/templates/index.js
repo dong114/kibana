7cd704e329ffa25e2dcc8dd827d358ab2988e067
Catherine Liu <catherine.liu@elastic.co>
2018-10-16 16:12:25 -0400

    Removed cloneDeep

--- x-pack/plugins/canvas/public/components/workpad_templates/templates/index.js
+++ x-pack/plugins/canvas/public/components/workpad_templates/templates/index.js
@@ -4,7 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { cloneDeep } from 'lodash';
 const canvasWeather = require('./canvas_weather.json');
 const conditionalAssets = require('./conditional_assets.json');
 const dublinDemo = require('./dublin_eah_workshop.json');
@@ -17,14 +16,14 @@ const operationalMonitoring = require('./sample_data_operational_monitoring.json
 const randomizedTour2018 = require('./randomized_tour_2018.json');
 
 export const templates = [
-  cloneDeep(canvasWeather),
-  cloneDeep(conditionalAssets),
-  cloneDeep(dublinDemo),
-  cloneDeep(ecommerceRevenueTracking),
-  cloneDeep(elasticTemplate),
-  cloneDeep(flightsOverview),
-  cloneDeep(logsWebTraffic),
-  cloneDeep(markdownStyles),
-  cloneDeep(operationalMonitoring),
-  cloneDeep(randomizedTour2018),
+  canvasWeather,
+  conditionalAssets,
+  dublinDemo,
+  ecommerceRevenueTracking,
+  elasticTemplate,
+  flightsOverview,
+  logsWebTraffic,
+  markdownStyles,
+  operationalMonitoring,
+  randomizedTour2018,
 ];
